1aa724955e5995249bee3a3b0745dcb0b9b884f1
wisherfox <wisherfox@gmail.com>
2025-12-15 04:07:13 +0100

    Background del nivel "Modulo_OldSchoolFloor" plataformas y interacción con ellas. *Pendiente de hablar sobre el spawn de elementos

---
+++ Assets/Scripts/PlataformDrops.cs
@@ -0,0 +1,11 @@
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class PlataformDrops : MonoBehaviour
+{
+    public Animator plataformController;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        plataformController.SetTrigger("onDrops");
+    }
+}
