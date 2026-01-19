fb50ed68a72b18ef2f81f78b5012e93ed14bf6ee
wisherfox <wisherfox@gmail.com>
2026-01-19 21:52:15 +0100

    Indicación de ir hacia arriba, fondo añadido a la escena de inicio, fixes en collectable

--- Assets/Scripts/CameraVerticalScroller.cs
+++ Assets/Scripts/CameraVerticalScroller.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class CameraVerticalScroller : MonoBehaviour
@@ -5,20 +7,33 @@ public class CameraVerticalScroller : MonoBehaviour
     public float startSpeed = 1f;
     public float maxSpeed = 5f;
     public float acceleration = 0.2f;
+    
+    [SerializeField] private TMP_Text text;
+
+    [SerializeField] int delayToStart = 1;
+    public bool canMove = false;
 
     private float currentSpeed;
 
     void Start()
     {
         currentSpeed = startSpeed;
+        StartCoroutine(ActivateCameraMovement());
     }
 
     void Update()
     {
-        // Aumentar velocidad progresivamente
+        if (!canMove) return;
+
         currentSpeed = Mathf.Min(currentSpeed + acceleration * Time.deltaTime, maxSpeed);
 
-        // Mover la cámara hacia arriba
         transform.position += Vector3.up * currentSpeed * Time.deltaTime;
+    }
+    IEnumerator ActivateCameraMovement()
+    {
+        yield return new WaitForSeconds(delayToStart);
+        canMove = true;
+        if (text != null)text.gameObject.SetActive(false);
+        
     }
 }
